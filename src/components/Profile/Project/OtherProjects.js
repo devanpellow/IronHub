04c70e0e06b0d27a06f7568f063a151cf48f88b7
Paul Hellweg <leonhard.hellweg@gmail.com>
2019-07-24 15:29:33 +0200

    working on ProjectPage and Components

---
+++ src/components/Profile/Project/OtherProjects.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class OtherProjects extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Project 1</h1>
+        </div>
+        <div>
+          <h1>Project 2</h1>
+        </div>
+        <div>
+          <h1>Project 3</h1>
+        </div>
+      </div>
+    );
+  }
+}
