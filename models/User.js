57385c66aadbd3a28bda7a1abb8e0ce495d24b34
Devan Pellow <devanpellow@gmail.com>
2019-07-25 20:43:43 +0200

    added Ironhack font family

--- models/User.js
+++ models/User.js
@@ -2,28 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-<<<<<<< HEAD
-  {
-    username: String,
-    password: String,
-    name: String,
-    bio: String,
-    github: String,
-    linkedin: String,
-    location: {
-      type: String,
-      enum: ["Berlin", "Lisbon", "Paris", "Madrid"]
-    },
-    skills: String
-    // projects: []
-  },
-  {
-    timestamps: {
-      createdAt: "created_at",
-      updatedAt: "updated_at"
-    }
-  }
-=======
 	{
 		username: String,
 		password: String,
@@ -44,7 +22,6 @@ const userSchema = new Schema(
 			updatedAt: "updated_at"
 		}
 	}
->>>>>>> 31551bec95aae96d812cfde3567872ef34250bc7
 );
 
 const User = mongoose.model("User", userSchema);
