44562969efd06d30d56ffa03b91bbf7b5fb430f9
Bjoern Seeber <bjoernseeber@yahoo.de>
2019-07-25 16:13:14 +0200

    Adjusted Imageupload

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -4,45 +4,43 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 export class ProjectList extends Component {
-	state = {
-		projectURL: ""
-	};
+  state = {
+    projectURL: ""
+  };
 
-	getProjectURL = () => {
-		const projectId = this.props.match.params.id;
+  // getProjectURL = () => {
+  //   const projectId = this.props.match.params.id;
 
-		return axios
-			.get(`/api/tasks/${projectId}`)
-			.then(response => {
-				const { projectURL } = response.data;
-				this.setState({ projectURL });
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	};
+  //   return axios
+  //     .get(`/api/tasks/${projectId}`)
+  //     .then(response => {
+  //       const { projectURL } = response.data;
+  //       this.setState({ projectURL });
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // };
 
-	componentDidMount() {
-		this.getProjectURL();
-	}
+  // componentDidMount() {
+  //   this.getProjectURL();
+  // }
 
-	render() {
-		const URL = {
-			projectURL: this.state.projectURL
-		};
-		return (
-			<div>
-				<Card style={{ width: "18rem" }}>
-					<Card.Body>
-						<img src="/../images/ironhub-logo.svg" alt="IronHub" />
-						<Card.Text>A Web App to do something cool.</Card.Text>
-							<Link to={URL.projectURL}>Visit Site</Link>
-						<Card.Link href="#">Project Details</Card.Link>
-					</Card.Body>
-				</Card>
-			</div>
-		);
-	}
+  render() {
+    //const url = this.state.projectURL;
+
+    return (
+      <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <img src="/../images/ironhub-logo.svg" alt="IronHub" />
+            <Card.Text>A Web App to do something cool.</Card.Text>
+            <Card.Link href="#">Project Details</Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default ProjectList;
