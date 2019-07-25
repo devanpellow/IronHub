231b7cf3513d61a266ac9884b92f2c25b9d2704c
Devan Pellow <devanpellow@gmail.com>
2019-07-25 16:15:00 +0200

    deleted error in player card

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,42 +1,42 @@
 import React, { Component } from "react";
 import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import axios from "axios";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
 
 export class ProjectList extends Component {
 	state = {
 		projectURL: ""
 	};
 
-	getProjectURL = () => {
-		const projectId = this.props.match.params.id;
+	// getProjectURL = () => {
+	// 	const projectId = this.props.match.params.id;
 
-		return axios
-			.get(`/api/tasks/${projectId}`)
-			.then(response => {
-				const { projectURL } = response.data;
-				this.setState({ projectURL });
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	};
+	// 	return axios
+	// 		.get(`/api/tasks/${projectId}`)
+	// 		.then(response => {
+	// 			const { projectURL } = response.data;
+	// 			this.setState({ projectURL });
+	// 		})
+	// 		.catch(err => {
+	// 			console.log(err);
+	// 		});
+	// };
 
-	componentDidMount() {
-		this.getProjectURL();
-	}
+	// componentDidMount() {
+	// 	this.getProjectURL();
+	// }
 
 	render() {
-		const URL = {
-			projectURL: this.state.projectURL
-		};
+		// const URL = {
+		// 	projectURL: this.state.projectURL
+		// };
 		return (
 			<div>
 				<Card style={{ width: "18rem" }}>
 					<Card.Body>
 						<img src="/../images/ironhub-logo.svg" alt="IronHub" />
 						<Card.Text>A Web App to do something cool.</Card.Text>
-							<Link to={URL.projectURL}>Visit Site</Link>
+						<Card.Link href="#">Visit Site</Card.Link>
 						<Card.Link href="#">Project Details</Card.Link>
 					</Card.Body>
 				</Card>
