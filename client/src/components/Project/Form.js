5b49f9c3d440cc5ff43b2f4991d7b38883c25953
Bjoern Seeber <bjoernseeber@yahoo.de>
2019-07-26 17:41:28 +0200

    Added image upload

--- client/src/components/Project/Form.js
+++ client/src/components/Project/Form.js
@@ -1,104 +1,116 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Form, Button } from "react-bootstrap";
+import AddImage from "../AddImage";
+// import service from ".../api/service";
 
 export default class ProjectForm extends Component {
-	state = {
-		title: "",
-		projectURL: "",
-		description: ""
-	};
+  state = {
+    title: "",
+    projectURL: "",
+    description: "",
+    module: "",
+    imageUrl: ""
+  };
 
-	handleChange = event => {
-		const { name, value } = event.target;
-		console.log(value);
-		this.setState({
-			[name]: value
-		});
-	};
+  handleChange = event => {
+    const { name, value } = event.target;
+    console.log(value);
+    this.setState({
+      [name]: value
+    });
+  };
 
-	handleSubmit = event => {
-		console.log("handleSubmit");
-		event.preventDefault();
+  handleSubmit = event => {
+    console.log("handleSubmit");
+    event.preventDefault();
 
-		axios
-			.post("/profile", {
-				title: this.state.title,
-				projectURL: this.state.projectURL,
-				description: this.state.description,
-				module: this.state.module
-			})
-			.then(() => {
-				// this.props.refreshList();
-				this.setState({
-					title: "",
-					projectURL: "",
-					description: "",
-					module: ""
-				});
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	};
+    axios
+      .post("/profile", {
+        title: this.state.title,
+        projectURL: this.state.projectURL,
+        description: this.state.description,
+        module: this.state.module,
+        imageUrl: this.state.imageUrl
+      })
+      .then(() => {
+        // this.props.refreshList();
+        this.setState({
+          title: "",
+          projectURL: "",
+          description: "",
+          module: "",
+          imageUrl: ""
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-	render() {
-		return (
-			<Form onSubmit={this.handleSubmit}>
-				<Form.Group>
-					<Form.Label>Module:</Form.Label>
-					<Form.Control
-						as="select"
-						onChange={this.handleChange}
-						type=""
-						name="module"
-						id="module"
-						value={this.state.module}
-					>
-						<option>Choose...</option>
-						<option value="1">Module 1</option>
-						<option value="2">Module 2</option>
-						<option value="3">Module 3</option>
-					</Form.Control>
-				</Form.Group>
-				{/* all groups (label + input) are grouped in a Form.Group */}
-				<Form.Group>
-					{/* <label></label> */}
-					<Form.Label htmlFor="title">Title: </Form.Label>
-					{/* <input /> */}
-					<Form.Control
-						type="text"
-						onChange={this.handleChange}
-						id="title"
-						name="title"
-						value={this.state.title}
-					/>
-				</Form.Group>
+  // this method handles just the file upload
+  handleFileUpload = imageUrl => {
+    this.setState({ imageUrl: imageUrl });
+  };
 
-				<Form.Group>
-					<Form.Label htmlFor="description">Description: </Form.Label>
-					<Form.Control
-						onChange={this.handleChange}
-						type="text"
-						name="description"
-						id="description"
-						value={this.state.description}
-					/>
-				</Form.Group>
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group>
+          <Form.Label>Module:</Form.Label>
+          <Form.Control
+            as="select"
+            onChange={this.handleChange}
+            type=""
+            name="module"
+            id="module"
+            value={this.state.module}
+          >
+            <option>Choose...</option>
+            <option value="1">Frontend</option>
+            <option value="2">Backend</option>
+            <option value="3">React</option>
+          </Form.Control>
+        </Form.Group>
+        {/* all groups (label + input) are grouped in a Form.Group */}
+        <Form.Group>
+          {/* <label></label> */}
+          <Form.Label htmlFor="title">Title: </Form.Label>
+          {/* <input /> */}
+          <Form.Control
+            type="text"
+            onChange={this.handleChange}
+            id="title"
+            name="title"
+            value={this.state.title}
+          />
+        </Form.Group>
 
-				<Form.Group>
-					<Form.Label htmlFor="projectURL">Project URL: </Form.Label>
-					<Form.Control
-						onChange={this.handleChange}
-						type="text"
-						name="projectURL"
-						id="projectURL"
-						value={this.state.projectURL}
-					/>
-				</Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="description">Description: </Form.Label>
+          <Form.Control
+            onChange={this.handleChange}
+            type="text"
+            name="description"
+            id="description"
+            value={this.state.description}
+          />
+        </Form.Group>
 
-				<Button type="submit">Add Project</Button>
-			</Form>
-		);
-	}
+        <Form.Group>
+          <Form.Label htmlFor="projectURL">Project URL: </Form.Label>
+          <Form.Control
+            onChange={this.handleChange}
+            type="text"
+            name="projectURL"
+            id="projectURL"
+            value={this.state.projectURL}
+          />
+        </Form.Group>
+        <AddImage onImageChange={this.handleFileUpload} />
+        {this.state.imageUrl && <img src={this.state.imageUrl} />}
+        <Button type="submit">Add Project</Button>
+      </Form>
+    );
+  }
 }
