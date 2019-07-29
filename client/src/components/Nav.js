b0fa1cad6f33d47197cf33d01556a9ec30b13c69
Paul Hellweg <leonhard.hellweg@gmail.com>
2019-07-29 15:29:21 +0200

    added png logo

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -6,52 +6,51 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 
 export default function Nav(props) {
-	// const handleSubmit = (event, history) => {
-	// 	event.preventDefault();
+  // const handleSubmit = (event, history) => {
+  // 	event.preventDefault();
 
-	// 	axios
-	// 		.get("/auth/logout")
-	// 		.then(data => {
-	// 			console.log(data);
-	// 			history.push("/");
-	// 		})
-	// 		.catch(err => {
-	// 			console.log("lul", err);
-	// 		});
-	// };
+  // 	axios
+  // 		.get("/auth/logout")
+  // 		.then(data => {
+  // 			console.log(data);
+  // 			history.push("/");
+  // 		})
+  // 		.catch(err => {
+  // 			console.log("lul", err);
+  // 		});
+  // };
 
-	return (
-		<div>
-			<Container id="nav-container">
-				<Navbar>
-					<Navbar.Brand href="/">
-						<img
-							src="/images/ironhub-logo.svg"
-							width="50"
-							height="50"
-							className="d-inline-block align-top"
-							alt="IronHub"
-						/>
-					</Navbar.Brand>
-					<Navbar.Toggle />
-					<Navbar.Collapse className="justify-content-end nav-btns">
-						<Link to="/login">
-							<Button variant="link">Login</Button>
-						</Link>
-						<Link to="/profile">
-							<Button variant="link">Profile</Button>
-						</Link>
-						<Link to="/signup">
-							<Button variant="link">Signup</Button>
-						</Link>
-				
-					</Navbar.Collapse>
-				</Navbar>
-			</Container>
-		</div>
-	);
+  return (
+    <div>
+      <Container id="nav-container">
+        <Navbar>
+          <Navbar.Brand href="/">
+            <img
+              src="/images/IRONHUBLOGO.png"
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+              alt="IronHub"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end nav-btns">
+            <Link to="/login">
+              <Button variant="link">Login</Button>
+            </Link>
+            <Link to="/profile">
+              <Button variant="link">Profile</Button>
+            </Link>
+            <Link to="/signup">
+              <Button variant="link">Signup</Button>
+            </Link>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    </div>
+  );
 }
 
-		// <a onClick={e => handleSubmit(e, props.history)}>
-						// 	<Button variant="link">Logout</Button>
-						// </a>
+// <a onClick={e => handleSubmit(e, props.history)}>
+// 	<Button variant="link">Logout</Button>
+// </a>
