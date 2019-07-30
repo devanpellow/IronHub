6b0398f21f29d227194ef6a1951bc212f3bf1b86
Bjoern Seeber <bjoernseeber@yahoo.de>
2019-07-30 13:48:47 +0200

    Added Like count

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -2,26 +2,11 @@ import React, { Component } from "react";
 // import ReactDOM from "react-dom";
 
 export default class LikeButton extends Component {
-  constructor() {
-    super();
-    this.state = {
-      liked: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState({
-      liked: !this.state.liked
-    });
-  }
-
   render() {
-    const text = this.state.liked ? "liked" : "haven't liked";
-    const label = this.state.liked ? "Unlike" : "Like";
+    const label = this.props.liked ? "Unlike" : "Like";
     return (
       <div className="customContainer">
-        <button className="btn btn-primary" onClick={this.handleClick}>
+        <button className="btn btn-primary" onClick={this.props.handleClick}>
           {label}
         </button>
       </div>
