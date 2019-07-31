0a116fda3f66bb9b1075943f46f2b051e29c4b22
Devan Pellow <devanpellow@gmail.com>
2019-07-31 10:24:51 +0200

    centered project cards

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const ProjectCard = props => {
-
-  const projects = props.filtered.length > 0 ? props.filtered : props.allProjects
+	const projects =
+		props.filtered.length > 0 ? props.filtered : props.allProjects;
 
 	return (
-		<div>
-			
-
+		<div className="projectCards">
 			{projects.map(project => {
 				return (
 					<Card key={project._id} style={{ width: "18rem" }}>
 						<Card.Body>
-							<img
-								className="projectImg"
-								src={`${project.imageUrl}`}
-								alt="project screenshot"
-							/>
-							<Card.Text>{project.title}</Card.Text>
+							<Link to={`/project/${project._id}`}>
+								<img
+									className="projectImg"
+									src={`${project.imageUrl}`}
+									alt="project screenshot"
+								/>
+								<Card.Text>{project.title}</Card.Text>
+							</Link>
 							<a
 								href={project.projectUrl}
+								rel="noopener noreferrer"
 								target="_blank"
 							>
 								Visit Site
@@ -38,28 +39,3 @@ const ProjectCard = props => {
 };
 
 export default ProjectCard;
-
-// {props.filtered.map(project => {
-//   return (
-//     <Card style={{ width: "18rem" }}>
-//       <Card.Body>
-//         <img
-//           className="projectImg"
-//           src={`${project.imageUrl}`}
-//           alt="project screenshot"
-//         />
-//         <Card.Text>{project.title}</Card.Text>
-//         <a
-//           href={project.projectUrl}
-//           rel="noopener noreferrer"
-//           target="_blank"
-//         >
-//           Visit Site
-//         </a>
-//         <Link to={`/project/${project._id}`}>
-//           Project Details
-//         </Link>
-//       </Card.Body>
-//     </Card>
-//   );
-// })}
