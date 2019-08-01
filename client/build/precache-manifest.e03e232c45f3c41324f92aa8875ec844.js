42064524be2c6482b02946798bddf81f81d9e3c3
Devan Pellow <devanpellow@gmail.com>
2019-08-02 01:23:21 +0200

    npm run build after styling changes

---
+++ client/build/precache-manifest.e03e232c45f3c41324f92aa8875ec844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9615cf7cd9688c7caf46d1a816bef7bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c180701afacb0bccfff4",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "dda20033258ac361399d",
+    "url": "/static/css/main.120c6572.chunk.css"
+  },
+  {
+    "revision": "c180701afacb0bccfff4",
+    "url": "/static/js/2.36ca66ab.chunk.js"
+  },
+  {
+    "revision": "dda20033258ac361399d",
+    "url": "/static/js/main.6e6a4b95.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
